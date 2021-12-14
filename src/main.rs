13734b616eff66af4b7065ecdb0edd92001c8d29
omgmajk <majkieboy@gmail.com>
2021-12-14 02:29:44 +0100

    added event handlers

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use ggez::{conf, event, Context, GameResult};
 use std::path;
 
 // This will hold our main game state
-struct Game{}
+struct Game {}
 
 // This is the main event loop, doing it the ggez way
 impl event::EventHandler<ggez::GameError> for Game {
     fn update(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
-    
+
     fn draw(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
@@ -18,13 +18,13 @@ impl event::EventHandler<ggez::GameError> for Game {
 pub fn main() -> GameResult {
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
-    .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
-    .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
-    .add_resource_path(path::PathBuf::from("./resources"));
+        .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
+        .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
+        .add_resource_path(path::PathBuf::from("./resources"));
 
     let (context, event_loop) = context_builder.build()?;
     // Create a game state
     let game = Game {};
-    // Run main event loop
+    // Run the main event loop
     event::run(context, event_loop, game)
 }
