db81ca3669d038bd20442543904068c84b7db673
omgmajk <majkieboy@gmail.com>
2021-12-14 02:19:43 +0100

    started main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{conf, event, Context, GameResult};
+use std::path;
+
+// This will hold our main game state
+struct Game{}
+
+// This is the main event loop, doing it the ggez way
+impl event::EventHandler<ggez::GameError> for Game {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
+    }
+    
+    fn draw(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    // Create a game context and event loop
+    let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
+    .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
+    .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
+    .add_resource_path(path::PathBuf::from("./resources"));
+
+    let (context, event_loop) = context_builder.build()?;
+    // Create a game state
+    let game = Game {};
+    // Run main event loop
+    event::run(context, event_loop, game)
 }
